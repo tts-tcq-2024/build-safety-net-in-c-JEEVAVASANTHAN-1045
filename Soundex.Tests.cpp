f215b0ff82bb14082c70c6254435c4f54f9a9337
JEEVAVASANTHAN-1045 <jiivavasanthan@gmail.com>
2024-08-12 10:51:29 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -30,13 +30,7 @@ TEST(SoundexTestsuite, HandlesVowelCharacters) {
     generateSoundex("AEIOUHWY", soundex);
     ASSERT_STREQ(soundex, "A000");
 }
-
-TEST(SoundexTestsuite, HandlesVowelCharacters) {
-    char soundex[5];
-  generateSoundex("Ferrari", soundex);
-  ASSERT_STREQ(soundex, "F600");
-}
-
+ 
 TEST(SoundexTestsuite, HandlesMultipleSameSoundexCodeCharacters) {
     char soundex[5];
     generateSoundex("YYYY", soundex);
